2e1385cce66b03433a220d5c1a4eddf2da12d799
Fluke Sikharin <sikharin@kkumail.com>
2025-01-26 13:57:21 +0700

    Added /product route

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ app.get('/', (req, res) => {
   res.send('5555555555!');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+app.get('/product', (req, res) => {
+    const product = {
+        id: 1,
+        name: 'Sample Product',
+        price: 100,
+        description: 'This is a sample product description.',
+    };
+    res.json(product);
+});
+
+const PORT = 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
